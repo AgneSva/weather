d76f4764c82094c7f0d9db61fc9ab7465c90d7ea
Agne Švambarytė <agnesvambaryte@192-168-10-246.local.balticum.lt>
2020-06-24 11:34:15 +0300

    added recommended section

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ var city = document.getElementById('city')
 var temp = document.getElementById('temp')
 var date = document.getElementById('date')
 
+//recommendations:
+var tempLN = document.getElementById('tempLN')
+var tempMS = document.getElementById('tempMS')
+var tempNY = document.getElementById('tempNY')
+
+
+
+
+
 searchIcon.addEventListener('click', () => {
     searchIcon.classList.add("hide")
     searchBox.classList.remove("hide")
@@ -27,11 +36,10 @@ submitBtn.addEventListener('click', function () {
 
             d = new Date();
             var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-            var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+           
 
             city.innerHTML = nameValue;
-            date.innerHTML = days[d.getDay()] + ", " + months[d.getMonth()] + " " + d.getDate() + "th " + d.getFullYear();
+            date.innerHTML =  months[d.getMonth()] + " " + d.getDate() + "th " + d.getFullYear();
             description.innerHTML = descValue;
             temp.innerHTML = tempValue + " °C";
             searchBox.value = "";
@@ -45,7 +53,68 @@ submitBtn.addEventListener('click', function () {
 })
 
 
+//get info for london
+fetch('https://api.openweathermap.org/data/2.5/weather?q=london&appid=6d2e15bc6c37daf2c82cb24bf5f5fde8')
+        .then(response => response.json())
+        .then(data => {
+            let tempLNvalue = data['main']['temp'];
+            var iconLNid = data['weather'][0]['icon'];
+
+            tempLNvalue -= 273.15
+            tempLNvalue = Math.round(tempLNvalue)
+
+
+          
+            tempLN.innerHTML = tempLNvalue + " °C";
+         
 
 
 
+            var iconUrl = "http://openweathermap.org/img/wn/" + iconLNid + "@2x.png";
+            document.getElementById("iconLN").src = iconUrl;
+
+        })
+//get info for moscow
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=moscow&appid=6d2e15bc6c37daf2c82cb24bf5f5fde8')
+        .then(response => response.json())
+        .then(data => {
+            let tempMSvalue = data['main']['temp'];
+            var iconMSid = data['weather'][0]['icon'];
+
+            tempMSvalue -= 273.15
+            tempMSvalue = Math.round(tempMSvalue)
+
+
+          
+            tempMS.innerHTML = tempMSvalue + " °C";
+         
+
+
+
+            var iconUrl = "http://openweathermap.org/img/wn/" + iconMSid + "@2x.png";
+            document.getElementById("iconMS").src = iconUrl;
+
+        })
+
+//get info for new york
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=new&york&appid=6d2e15bc6c37daf2c82cb24bf5f5fde8')
+        .then(response => response.json())
+        .then(data => {
+            let tempNYvalue = data['main']['temp'];
+            var iconNYid = data['weather'][0]['icon'];
+
+            tempNYvalue -= 273.15
+            tempNYvalue = Math.round(tempNYvalue)
+
+
+          
+            tempNY.innerHTML = tempNYvalue + " °C";
+         
+
+
+
+            var iconUrl = "http://openweathermap.org/img/wn/" + iconNYid + "@2x.png";
+            document.getElementById("iconNY").src = iconUrl;
+
+        })
 
